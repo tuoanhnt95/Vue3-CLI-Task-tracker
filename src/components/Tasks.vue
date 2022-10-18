48b7e15e7e7d114465e6cadd4d1889f17ade954b
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-18 14:42:32 +0900

    feat: Add task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,19 @@
 <template>
     <div v-for="task in tasks" :key="task.id">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task"/>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Tasks',
-        props: {
-            tasks: Array,
-        },
-    }
+import Task from './Task.vue';
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: {
+    Task,
+}
+}
 </script>
